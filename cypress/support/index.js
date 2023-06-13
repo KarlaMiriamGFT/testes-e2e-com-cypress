2b6a2917a2c447ffcb7293a928c07865b825c8ce
KarlaMiriam <kmds@gft.com>
2023-06-13 13:30:17 -0300

    Alteração do projeto

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,3 +2,8 @@ import 'cypress-mailosaur'
 import './commands'
 import 'cypress-file-upload'
 import 'cypress-iframe'
+
+module.exports = (on, config) => {
+    require('cypress-grep/src/plugin')(config)
+  }
+  require('cypress-grep')()
